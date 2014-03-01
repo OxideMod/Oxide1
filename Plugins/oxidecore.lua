9d73865cce69532d71642ae45de56ef87bc3c88e
Lucien Murray-Pitts <lucien@rustoxide.com>
2014-03-01 15:03:59 +0900

    Door swinging in both directions fixed
    Simple empty else statement fix

--- Plugins/oxidecore.lua
+++ Plugins/oxidecore.lua
@@ -209,7 +209,7 @@ function PLUGIN:OnDoorToggle( door, timestamp, controllable )
 			print( "eyesOrigin was a string! (charcomponent = " .. tostring( charcomponent ) .. " - " .. (charcomponent and charcomponent:GetType().Name or "") .. ")" )
 			origin = GetEyesOrigin( charcomponent )
 		else
-			origin = GetEyesOrigin( charcomponent ) -- LMP fixes for door swinging both ways, probably could also use charcomponent.eyesOrigin but this is safer.
+			
 		end
 	else
 		origin = controllable.transform.position
